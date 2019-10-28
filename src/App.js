3c5f8d3ddb5538dae55bb11b1f5f91e4aac8e6e6
[Brandon Teague] <[teaguebrandon@outlook.com]>
2019-10-28 15:56:17 -0400

    set up state and rendered components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  ////Set all state and state changes in here
+
+  constructor() {
+    super();
+    this.state = {
+      todos: [] /// Setting The todos to an empty array that I can add to
+    };
+  }
+
+  addTodo = name => {
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
+  filterCompleted = () => {};
+
+  toggleCompleted = () => {};
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <div>
+          <TodoForm />
+          <TodoList />
+        </div>
       </div>
     );
   }
